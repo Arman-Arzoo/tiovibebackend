93a6fd8f6a848e8ba8bb4cf8871026351085eef2
Muhammad Umair <toplinegeeks@gmail.com>
2024-02-23 19:35:52 +0500

    update

---
+++ services/db.js
@@ -0,0 +1,38 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    console.log('Connected to MySQL database!');
+});
+
+async function executeQuery(query, values) {
+    try {
+        const results = await new Promise((resolve, reject) => {
+            connection.query(query, values, (err, res) => {
+                if (err) {
+                    console.log('Error in database:', err);
+                    reject(err); // Reject with error
+                    return;
+                }
+                resolve(res); // Resolve with results
+            });
+        });
+        return results;
+    } catch (error) {
+        console.error('executeQuery error:', error);
+        throw error;
+    }
+}
+
+module.exports = {
+    connection,
+    executeQuery
+};
